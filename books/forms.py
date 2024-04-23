2d158bc4334e4a412896aae5554c62b32752a95c
Pietrek-K <pkusnierz95@protonmail.com>
2024-04-23 09:41:04 +0200

    Improve form for add book by adding placeholder and change date_realese input from text to date

--- books/forms.py
+++ books/forms.py
@@ -1,9 +1,11 @@
+from re import L
 from django.forms import ModelForm, widgets
 from django import forms
 from .models import Book
 
 
 class BookForm(ModelForm):
+    tags = forms.CharField(label="Tagi", widget=forms.TextInput)
     class Meta:
         model = Book
         # fields = "__all__"
@@ -13,6 +15,7 @@ class BookForm(ModelForm):
             "title":"Tytuł",
             "author":"Autor",
             "category":"Kategoria",
+            "tags":"Tagi",
             "description":"Opis",
             "source":"Źródło",
             "publisher":"Wydawca",
@@ -28,4 +31,11 @@ class BookForm(ModelForm):
         
         self.fields['image'].widget.attrs['class'] = "addbook__basicInfo__inputFile--button"
         self.fields['image'].widget.attrs['id'] = "upload"
-    
+        self.fields['date_realease'].widget = forms.DateInput(attrs={'type': 'date'})
+        self.fields['title'].widget.attrs['placeholder'] = "Tytuł"
+        self.fields['author'].widget.attrs['placeholder'] = "Autor"
+        self.fields['tags'].widget.attrs['placeholder'] = "Tag1, Tag2, "
+        self.fields['description'].widget.attrs['placeholder'] = "Ksiązka opowiada o ......"
+        self.fields['publisher'].widget.attrs['placeholder'] = "Wydawca"
+        self.fields['isbn'].widget.attrs['placeholder'] = "1234567890123"
+
